940de932a81208868e592c0449f610ad277a49fd
chilliflakes5790 <thakaliaayush@gmail.com>
2025-07-18 17:02:36 +0545

    feat:components added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import CarForm from "./components/CarForm"
+import CarList from "./components/CarList"
+import CarValue from "./components/CarValue"
+import CarSearch from "./components/CarSearch"
+
+function App() {
+  return (
+    <div>
+        <CarForm/>
+        <CarSearch/>
+        <CarValue/>
+        <CarList/>
+    </div>
+  )
+}
+
+export default App
